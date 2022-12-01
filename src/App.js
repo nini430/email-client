418100b88077f6bea4f84a9669a58e78f4d6546b
ngogatishvili <ngogatishvili@websmart.gmail.com>
2022-12-01 15:13:59 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
+import { MessageSystem } from "./pages/MessageSystem";
+import { SetName } from "./pages/SetName";
+import { UserContextProvider } from "./context/UserContext";
+
+const router = createBrowserRouter([
+  {
+    path: "/setName",
+    element: <SetName />,
+  },
+  {
+    path: "/",
+    element: <MessageSystem />,
+  },
+]);
 function App() {
+  const client = new QueryClient();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={client}>
+      <UserContextProvider>
+        <RouterProvider router={router} />
+      </UserContextProvider>
+    </QueryClientProvider>
   );
 }
 
